b73c02cad8509ee8685d1ab296a8b5de4fd0da52
Simon Nolet <rastanolet@gmail.com>
2020-09-05 14:58:03 -0400

    Premier commit

---
+++ app/src/main/java/com/example/simon_nolet_tp1_v0_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.simon_nolet_tp1_v0_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
